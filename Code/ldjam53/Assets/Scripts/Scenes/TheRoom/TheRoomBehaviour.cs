e3979bb7e173c6c184b91f149e2f25c39300db67
Rentem <salasam@gmx.net>
2023-04-29 15:50:00 +0200

    tt

--- Code/ldjam53/Assets/Scripts/Scenes/TheRoom/TheRoomBehaviour.cs
+++ Code/ldjam53/Assets/Scripts/Scenes/TheRoom/TheRoomBehaviour.cs
@@ -15,6 +15,7 @@ namespace Assets.Scripts.Scenes.TheRoom
         private Int32 counter = 0;
 
         public GameObject template;
+        public Camera sceneCamera;
 
         public List<GameObject> models;
 
@@ -42,8 +43,42 @@ namespace Assets.Scripts.Scenes.TheRoom
 
         public void TestLoad()
         {
-            var room = Base.Core.Game.AvailableGameModes[0].TheRoom;
+            //var room = Base.Core.Game.AvailableGameModes[0].TheRoom;
+            var room = BuildRoom();
             LoadRoom(room);
+        }
+
+        public Room BuildRoom()
+        {
+            int sizeX = 10;
+            int sizeY = 10;
+            int sizeZ = 10;
+            Room room = new Room
+            {
+                Materials = new RoomElement[sizeX, sizeY, sizeZ]
+            };
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    room.Materials[x, y, 0] = new RoomElement("Wall");
+                    room.Materials[x, y, sizeZ - 1] = new RoomElement("Wall");
+                }
+                for (int z = 0; z < sizeZ; z++)
+                {
+                    room.Materials[x, 0, z] = new RoomElement("Wall");
+                    room.Materials[x, sizeY - 1, z] = new RoomElement("Wall");
+                }
+            }
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int z = 0; z < sizeZ; z++)
+                {
+                    room.Materials[0, y, z] = new RoomElement("Wall");
+                    room.Materials[sizeX - 1, y, z] = new RoomElement("Wall");
+                }
+            }
+            return room;
         }
 
         public void LoadBox()
@@ -118,13 +153,14 @@ namespace Assets.Scripts.Scenes.TheRoom
                             var mat = Instantiate(template, template.transform.parent);
 
                             mat.AddRoomElement(roomElement);
-
+                            mat.AddComponent<BoundsCalculationBehaviour>();
                             mat.transform.position = new UnityEngine.Vector3(x, y, z);
                             mat.SetActive(true);
                         }
                     }
                 }
             }
+            AdjustCamera();
         }
 
         private void AddRoomElement(RoomElement roomElement, Vector3 position, Boolean setSelected = true)
@@ -154,6 +190,60 @@ namespace Assets.Scripts.Scenes.TheRoom
             this.selectedElement.SetSelected(true);
         }
 
+
+        public void AdjustCamera()
+        {
+            Bounds b = GetBounds(this.gameObject);
+
+            float cameraDistance = .25f; // Constant factor
+            Vector3 objectSizes = b.max - b.min;
+
+            float objectSize = Mathf.Max(objectSizes.x, objectSizes.y, objectSizes.z);
+
+            float cameraView = 2.0f * Mathf.Tan(0.5f * Mathf.Deg2Rad * sceneCamera.fieldOfView); // Visible height 1 meter in front
+            float distance = cameraDistance * objectSize / cameraView; // Combined wanted distance from the object
+            distance += 0.5f * objectSize; // Estimated offset from the center to the outside of the object
+            sceneCamera.transform.position = b.center - distance * sceneCamera.transform.forward;
+        }
+
+        internal static Bounds GetBounds(GameObject gameObject)
+        {
+            Bounds b = new Bounds(gameObject.transform.position, Vector3.zero);
+
+            b = GetBoundRec(gameObject.transform, b);
+
+            return b;
+        }
+
+        internal static Bounds GetBoundRec(Transform goT, Bounds b)
+        {
+            var boundsBehaviour = goT.GetComponent<BoundsCalculationBehaviour>();
+
+            if (boundsBehaviour == default || boundsBehaviour.IsIncluded)
+            {
+                foreach (Transform child in goT)
+                {
+                    if (child.gameObject.activeSelf)
+                    {
+                        b = GetBoundRec(child, b);
+
+                        Renderer r = child.GetComponent<MeshRenderer>();
+
+                        if (r != default)
+                        {
+                            b.Encapsulate(r.bounds);
+                            //Debug.Log("dodi", child);
+                        }
+                        else
+                        {
+                            //Debug.Log("No render:", child);
+                        }
+                    }
+                }
+            }
+
+            return b;
+        }
 
         //private void LateUpdate()
         //{
