511f82eefed8a624fe84c2d205b9e21158e3ebb3
XpArKeR <xparker@gmx.net>
2023-05-01 13:23:06 +0200

    Improved SavedGames.

--- Code/ldjam53/Assets/Scripts/Menues/SavedGames/SavedGameDetailBehaviour.cs
+++ Code/ldjam53/Assets/Scripts/Menues/SavedGames/SavedGameDetailBehaviour.cs
@@ -11,7 +11,9 @@ namespace Assets.Scripts.Scene.SaveGame
 
         private Text createdOn;
         private Text timeStamp;
-        private Text timeElapsed;
+        private Text completedLevels;
+        private Text currentLevel;
+        private Text owlType;
 
         [SerializeField]
         private GameObject container;
@@ -22,7 +24,10 @@ namespace Assets.Scripts.Scene.SaveGame
         {
             createdOn = transform.Find("DetailsContainer/Created/Value").GetComponent<Text>();
             timeStamp = transform.Find("DetailsContainer/TimeStamp/Value").GetComponent<Text>();
-            timeElapsed = transform.Find("DetailsContainer/TimeElapsed/Value").GetComponent<Text>();
+            completedLevels = transform.Find("DetailsContainer/CompletedLevels/Value").GetComponent<Text>();
+            currentLevel = transform.Find("DetailsContainer/CurrentLevel/Value").GetComponent<Text>();
+            owlType = transform.Find("DetailsContainer/OwlType/Value").GetComponent<Text>();
+
             var overwriteButton = transform.Find("DetailsContainer/Buttons/Override").GetComponent<Button>();
 
             overwriteButton.gameObject.SetActive(Base.Core.Game.State != default);
@@ -33,7 +38,9 @@ namespace Assets.Scripts.Scene.SaveGame
         {
             createdOn.text = preview.CreatedOn;
             timeStamp.text = preview.SavedOn;
-            timeElapsed.text = preview.TimeElapsed;
+            completedLevels.text = preview.CompletedLevelsCount;
+            currentLevel.text = preview.CurrentLevel;
+            owlType.text = preview.OwlType;
 
             this.key = preview.Key;
             container.SetActive(true);
